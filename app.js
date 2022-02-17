7231925c6c231d2cfa9f01e57d4b92b134b45f6a
Zeroska <owlthres@gmail.com>
2022-02-17 20:29:51 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require("cors")
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const app = express()
+const PORT = 3000
+
+// Firebstore account setup
+const serviceAccount = require('./gdsc-gateway-service-account.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+// Route import
+
+// Middle Ware
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+// Api route
+
+
+
+// Server config
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
