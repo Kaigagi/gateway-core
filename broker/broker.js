f51d0f42b2145e441b27218370d0b4c855102336
DangCao1999 <cao.nvd2246@sinhvien.hoasen.edu.vn>
2022-02-24 18:05:40 +0700

    authentication connect to broker

--- broker/broker.js
+++ broker/broker.js
@@ -1,22 +1,30 @@
 const aedes = require('aedes');
 const broker = aedes();
-
-
 const server = require('net').createServer(broker.handle)
-const port = 1883
 
-server.listen(port, function () {
-  console.log('server started and listening on port ', port)
-})
+let usernameBroker = "GDSCHSU";
+let passwordBroker = "Mailaanhem123";
 
-broker.on('clientReady', async function(client){
-  console.log("--------clientReady");
-  console.log(client.id);
+broker.authenticate = function (client, username, password, callback) {
+    let test = password.toString("utf8");
+    if (username != usernameBroker || test != passwordBroker) {
+        var error = new Error('Auth error')
+        error.returnCode = 4
+        callback(error, null)
+        return;
+    }
+    callback(null, true);
+}
+
+
+broker.on('clientReady', async function (client) {
+    console.log("--------clientReady");
+    console.log(client.id);
 });
 
-broker.on("clientDisconnect", async function(client){
-  console.log("-------clientDisconnect");
-  console.log(client);
+broker.on("clientDisconnect", async function (client) {
+    console.log("-------clientDisconnect");
+    console.log(client);
 })
 
 
