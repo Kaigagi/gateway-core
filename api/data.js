0713343b742aa498959fb0bc5f83e481bc07e050
truonghoangduy <truonghoangduy03111999@gmail.com>
2022-02-25 23:23:00 +0700

    hot fix using constance

--- api/data.js
+++ api/data.js
@@ -4,19 +4,19 @@ const { headerConstants } = require("../config/constants/header_constants")
 const { postDeviceSensorData } = require("../services/data")
 const router = express.Router()
 
-router.get("/data",(req,res) => {
+router.get("/data", (req, res) => {
 
 })
 
-router.post("/data",async (req,res) => {
-    try { 
+router.post("/data", async (req, res) => {
+    try {
         // Must have Access Key and Api key 
-        let accessKey = req.get("accessKey")
-        let apiKey = req.get("apiKey")
-        
+        let accessKey = req.get(headerConstants.deviceKeyHeader)
+        let apiKey = req.get(headerConstants.apiKeyHeader)
+
         // TODO: Check the FORMAT of the accessKey and API key (does it in the pool of the key or not)
-        if(accessKey === null || accessKey === undefined){
-            if(accessKey){}
+        if (accessKey === null || accessKey === undefined) {
+            if (accessKey) { }
             res.sendStatus(403)
         }
         // Pass logic to the data service, using function postDeviceSensorData
@@ -28,10 +28,10 @@ router.post("/data",async (req,res) => {
             req.body.isComplete
         )
         res.sendStatus(200)
-    }catch(err) {
-        if(err.message === "wrong datatype" || err.message === "wrong value"){
+    } catch (err) {
+        if (err.message === "wrong datatype" || err.message === "wrong value") {
             res.sendStatus(404)
-        }else{
+        } else {
             console.log(err)
             res.sendStatus(500)
         }
