867bd7b7ce45d0ed7cc1df2fd53c0210334bf167
Zeroska <owlthres@gmail.com>
2022-02-28 11:46:33 +0700

    Add dotenv and move all the constant value to .env

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const cors = require("cors");
-
+const dotven = require("dotenv").config()
 const admin = require('firebase-admin');
 const winston = require('winston');
 const expressWinston = require('express-winston');
 const app = express();
 
-const APP_PORT = 3000;
-const BROKER_PORT = 1883;
-const VERSION = "v1";
 
 // Firebstore account setup
-const serviceAccount = require('./gdsc-gateway-firebase.json');
+const serviceAccount = require(process.env.SERVICE_ACCOUNT_PATH);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 })
 
 // Route import
-const helloRoute = require("./api/test.js")
 const organizationRoute = require("./api/organization.js")
 const deviceRoute = require("./api/device.js")
 const dataRoute = require("./api/data.js");
@@ -49,19 +45,39 @@ app.use(express.urlencoded())
 app.use(cors())
 
 // Api route
-
-app.use(`/api/${VERSION}`,helloRoute)
-app.use(`/api/${VERSION}`,organizationRoute)
-app.use(`/api/${VERSION}`,deviceRoute)
-app.use(`/api/${VERSION}`,dataRoute)
-
+app.use(`/api/${process.env.API_VERSION}`,organizationRoute)
+app.use(`/api/${process.env.API_VERSION}`,deviceRoute)
+app.use(`/api/${process.env.API_VERSION}`,dataRoute)
+// Heath Check API
+app.get('/_health', (req,res)=>{
+  res.status(200).send("OK")
+})  
 
 //Broker
-server.listen(BROKER_PORT, function () {
-  console.log('Broker started and listening on port', BROKER_PORT);
+const brokerServer = server.listen(process.env.BROKER_PORT, function () {
+  console.log('Broker started and listening on port',process.env.BROKER_PORT);
 })
 
 // Server config
-app.listen(APP_PORT, () => {
-    console.log('Server Gateway listening on port', APP_PORT)
+const expressServer = app.listen(process.env.EXPRESS_PORT, () => {
+    console.log('Server Gateway listening on port', process.env.EXPRESS_PORT)
 })
+
+// Shutdown gracefully
+const startGracefulShutdown = ()=>{
+  console.log("Closing http & broker server")
+  // read the doc of close() and you will understand control C and control Z doesn't work anymore
+  expressServer.close(()=>{
+    console.log("Http server closed")
+  })
+
+  // Should read more of these 
+  // 1: https://hackernoon.com/graceful-shutdown-in-nodejs-2f8f59d1c357
+  // 2: https://blog.heroku.com/best-practices-nodejs-errors
+  brokerServer.close(()=>{
+    console.log("Broker server closing")
+    process.exit(1  )
+  })
+}
+process.on('SIGTERM', startGracefulShutdown)
+process.on('SIGINT', startGracefulShutdown)
